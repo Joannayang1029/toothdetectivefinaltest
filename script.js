9a2052f74988b2c33237d78204a453ad9d654526
Joannayang1029 <joannayang1029@gmail.com>
2026-05-22 02:17:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -446,15 +446,15 @@ function showCurrentPage() {
             html += `
                 <tr style="height: ${rowHeight};">
                     <!-- 題號（位置固定） -->
-                    <td style=" padding: 16px 8px; width: 5%; text-align:center; font-weight:900; vertical-align:middle; color:${rowColor};">
+                    <td style=" padding: 16px 4px; width: 5%; text-align:center; font-weight:900; vertical-align:middle; color:${rowColor};">
                         ${index+1}
                     </td>
                     <!-- 題目（右移 + 加寬） -->
-                    <td style=" padding: 16px 25px; width: 20%; text-align:left; vertical-align:middle; color:${rowColor};">
+                    <td style=" padding: 16px 10px; width: 20%; text-align:left; vertical-align:middle; color:${rowColor};">
                         ${q.question}
                     </td>
                     <!-- 答案（右移 + 加寬） -->
-                    <td style="padding: 16px 25px; width: 15%; text-align:left; font-weight:900; vertical-align:middle; color:${rowColor};">
+                    <td style="padding: 16px 10px; width: 15%; text-align:left; font-weight:900; vertical-align:middle; color:${rowColor};">
                         ${userAnswerText}
                     </td>
                 </tr>
